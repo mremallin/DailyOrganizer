2605f885c5d853bcff3675b685c91a8931360cdf
Mike Mallin <mike@mremallin.ca>
2023-05-07 14:34:52 -0400

    Support creating meeting notes from a template
    Squashed commit of the following:
    
    commit 18e730a7b1555ddf7c0f6c50aa990ea48aaced25
    Author: Mike Mallin <mike@mremallin.ca>
    Date:   Sun May 7 14:34:28 2023 -0400
    
        Add PackageControl messages
    
    commit 7540fa4162006b9f947790c5f60e40771d03e879
    Author: Mike Mallin <mike@mremallin.ca>
    Date:   Sun May 7 14:29:14 2023 -0400
    
        Support copying the chosen template to the new meeting file
    
    commit 6a386bbc6bf846eed145e7dd8251075938652bab
    Author: Mike Mallin <mike@mremallin.ca>
    Date:   Sun May 7 13:57:06 2023 -0400
    
        List user provided templates
    
    commit 1894f8b88461edda739ab5ca9ddb993439caee9c
    Author: Mike Mallin <mike@mremallin.ca>
    Date:   Sun May 7 13:30:39 2023 -0400
    
        Add empty list view for a template folder

--- DailyOrganizer.py
+++ DailyOrganizer.py
@@ -1,6 +1,6 @@
 #
 # DailyOrganizer - A simple daily notes organizer for Sublime Text
-# Copyright (C) 2021 Mike Mallin
+# Copyright (C) 2021-2023 Mike Mallin
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,6 +18,7 @@
 import sublime
 import sublime_plugin
 import os
+import shutil
 
 from functools import wraps
 from datetime import datetime
@@ -31,14 +32,22 @@ def get_folder_structure():
     settings = sublime.load_settings("DailyOrganizer.sublime-settings")
     return settings.get("folder_structure", "~/DailyOrganizer/%Y/%m/%d")
 
+
 def get_main_notes_file_name():
     settings = sublime.load_settings("DailyOrganizer.sublime-settings")
     return settings.get("main_notes_file_name", "Today.txt")
+
+
+def get_user_meeting_templates_folder():
+    settings = sublime.load_settings("DailyOrganizer.sublime-settings")
+    return settings.get("meeting_template_folder", "~/DailyOrganizer/MeetingTemplates")
+
 
 def create_folder(folder):
     if not os.path.exists(folder):
         print("Creating notes folder:" + folder)
         os.makedirs(folder)
+
 
 def get_current_folder():
     folder_path = datetime.now().strftime(get_folder_structure())
@@ -46,6 +55,7 @@ def get_current_folder():
 
     create_folder(folder_path)
     return folder_path
+
 
 def get_current_file():
     return datetime.now().strftime(get_main_notes_file_name())
@@ -57,8 +67,14 @@ def create_note_file(file_name):
         note_file = open(file_name, "x")
         note_file.close()
 
+
+def copy_template_into_note(note_file_name, template_file):
+    shutil.copyfile(template_file, note_file_name)
+
+
 def get_todays_note_file_path():
     return (get_current_folder() + "/" + get_current_file())
+
 
 class OpenTodaysNoteCommand(sublime_plugin.WindowCommand):
     def run(self):
@@ -76,17 +92,46 @@ def got_meeting_file_name(file_name):
     new_meeting_obj.create_meeting_file(file_name)
     new_meeting_obj = None
 
+
 def cancel_meeting_file_name():
     new_meeting_obj = None
 
+
+class MeetingTemplateChooserCommand(sublime_plugin.ListInputHandler):
+    def get_list_of_user_meeting_template_files(self):
+        user_templates_folder = get_user_meeting_templates_folder()
+        user_templates_folder = os.path.expanduser(user_templates_folder)
+
+        user_templates = [os.path.join(dirpath,f) for (dirpath, dirnames, filenames) \
+                            in os.walk(user_templates_folder) for \
+                                f in filenames]
+        return user_templates
+
+
+    def list_items(self):
+        return sorted(self.get_list_of_user_meeting_template_files()) + ["None"]
+
+
 class NewMeetingNoteCommand(sublime_plugin.WindowCommand):
-    def run(self):
+    def input(self, args):
+        return MeetingTemplateChooserCommand()
+
+
+    def run(self, meeting_template_chooser_command):
         global new_meeting_obj
+        self.chosen_meeting_template = meeting_template_chooser_command
         new_meeting_obj = self
         meeting_file_name_panel = self.window.show_input_panel(
             "Meeting File Name", "", got_meeting_file_name, None, cancel_meeting_file_name)
 
+
     def create_meeting_file(self, file_name):
         meeting_file_path = (get_current_folder() + "/" + file_name)
-        create_note_file(meeting_file_path)
+
+        # Make sure we don't overwrite user data with a template accidentally
+        if not os.path.exists(file_name):
+            create_note_file(meeting_file_path)
+            if self.chosen_meeting_template != "None":
+                copy_template_into_note(meeting_file_path, self.chosen_meeting_template)
         meeting_note = self.window.open_file(meeting_file_path)
+
--- README.md
+++ README.md
@@ -9,7 +9,11 @@ Once installed, there are two new options added:
 This will either create or open a note file for today. The default location of the note will be at `~/DailyOrganizer/%Y/%m/%d/Today.txt`
 
 ## Create a new meeting minutes note
-This will prompt the user for a file name to store the new meeting minutes in the same folder as `Today.txt`
+This will first prompt the user to choose one of their available templates (Default at `~/DailyOrganizer/MeetingTemplates`) and then to choose a file name to store the new meeting minutes in the same folder as `Today.txt`.
+
+The contents of the template file will be copied into the meeting file when it is first created. If the file already exists, it will remain untouched.
+
+The "None" template is special and indicates that a blank file will be created.
 
 # Configuration
 ## folder_structure
@@ -17,3 +21,6 @@ This is the folder layout of how to store the note files. It accepts formatting 
 
 ## main_notes_file_name
 This is the name of the singular note file in each directory. This also accepts formatting specifiers from strftime.
+
+## meeting_template_folder
+This is the folder which stores meeting templates. The folder and all subdirectories will be searched for any files to be used as a template. The chosen template is copied without change into the newly created meeting file.
